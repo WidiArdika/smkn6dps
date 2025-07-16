cab3d2e4cd5822283accbc603d163a88d1e52054
WidiArdika <dewidiardika@gmail.com>
2025-07-16 22:01:15 +0800

    Kembali ke awal

--- app/Filament/Resources/JurusanResource.php
+++ app/Filament/Resources/JurusanResource.php
@@ -19,7 +19,6 @@ use App\Filament\Resources\JurusanResource\Pages;
 use App\Helpers\ImageHelper;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\JurusanResource\RelationManagers;
-use App\Traits\SyncsUploadedFileToPublic;
 
 class JurusanResource extends Resource
 {
@@ -35,8 +34,6 @@ class JurusanResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-book-open';
     protected static ?string $activeNavigationIcon = 'heroicon-s-book-open';
 
-    use SyncsUploadedFileToPublic;
-
     public static function form(Form $form): Form
     {
         return $form
@@ -51,9 +48,6 @@ class JurusanResource extends Resource
                     ->disk('public')
                     ->directory('jurusan-images')
                     ->required()
-                    ->afterStateUpdated(function ($state) {
-                        static::syncToPublicStorage('jurusan-images', $state);
-                    })
                     ->acceptedFileTypes(['image/jpeg', 'image/png', 'image/jpg', 'image/webp'])
                     ->maxSize(2048)
                     ->helperText(new HtmlString(
